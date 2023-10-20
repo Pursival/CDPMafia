feb9942dd96b006f8df05f5a649d7821b162c8cb
Crazzyninja <ivodivo@mail.bg>
2023-10-20 16:48:58 +0300

    Importing Templates for Marketplace

--- cdp-mafia/src/components/marketplace/MarketPage.js
+++ cdp-mafia/src/components/marketplace/MarketPage.js
@@ -49,45 +49,28 @@ export default function MarketPage() {
             </AppBar>
             <main>
                 {/* Hero unit */}
-                <Box
-                    sx={{
-                        bgcolor: 'background.paper',
-                        pt: 8,
-                        pb: 6,
-                    }}
-                >
-                    <Container maxWidth="sm">
-                        <Typography
-                            component="h1"
-                            variant="h2"
-                            align="center"
-                            color="text.primary"
-                            gutterBottom
-                        >
-                            Album layout
-                        </Typography>
-                        <Typography variant="h5" align="center" color="text.secondary" paragraph>
-                            Something short and leading about the collection below—its contents,
-                            the creator, etc. Make it short and sweet, but not too short so folks
-                            don&apos;t simply skip over it entirely.
-                        </Typography>
-                        <Stack
-                            sx={{ pt: 4 }}
-                            direction="row"
-                            spacing={2}
-                            justifyContent="center"
-                        >
-                            <Button variant="contained">Main call to action</Button>
-                            <Button variant="outlined">Secondary action</Button>
-                        </Stack>
-                    </Container>
-                </Box>
+
                 <Container sx={{ py: 8 }} maxWidth="md">
                     {/* End hero unit */}
                     <Grid container spacing={4}>
+                        <Grid item xs={12} sm={6} md={4}>
+                            <ArticleForGrid/>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={4}>
+                            <ArticleForGrid/>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={4}>
+                            <ArticleForGrid/>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={4}>
+                            <ArticleForGrid/>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={4}>
+                            <ArticleForGrid/>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={4}>
                         <ArticleForGrid/>
-                        <ArticleForGrid/>
-                        <ArticleForGrid/>
+                    </Grid>
                     </Grid>
                 </Container>
             </main>
